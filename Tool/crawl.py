ffbf45abc0801c6468e97aa538c854c3e591af32
alan <alan@i3fresh.tw>
2019-07-31 14:47:32 +0800

    調整爬蟲

--- Tool/crawl.py
+++ Tool/crawl.py
@@ -12,11 +12,6 @@ mydb = mysql.connector.connect(
 )
 mycursor = mydb.cursor(dictionary=True)
 
-# def insert(conn, tableName, data):
-#     sql = "INSERT INTO " + tableName + " (name, url) VALUES (%s, %s)"
-#     val = ("RUNOOB", "https://www.runoob.com")
-#     conn.execute(sql, val)
-
 # dateStr = datetime.datetime(2018,1,2).strftime('%Y%m%d')
 domain = 'https://www.ptt.cc'
 beautyIndex = domain + '/bbs/Beauty/index.html'
@@ -31,7 +26,6 @@ soup = BeautifulSoup(r.text, 'html.parser')
 # # 網頁標題 HTML 標籤
 # titleTag = soup.title
 aTags = soup.find_all('a')
-# links = []
 for tag in aTags:
     tagText = tag.string
     if (tagText is not None and (tagText.find('[正妹]') != -1 or tagText.find('[神人]') != -1)):
@@ -46,7 +40,7 @@ for tag in aTags:
                 mycursor.execute("INSERT INTO `Tag` SET `title` = %s", (tagTitle, ))
                 mydb.commit()
                 tagId = mycursor.lastrowid
-                print(mycursor.lastrowid, "新增 title " + tagTitle + "成功")
+                print(mycursor.lastrowid, "新增 title " + tagTitle + " 成功")
             else:
                 tagId = tagRes['id']
 
@@ -54,11 +48,11 @@ for tag in aTags:
         mycursor.execute("SELECT * FROM `CrawlUrl` WHERE `url` = %s", (tagHref, ))
         crawlUrlRes = mycursor.fetchone()
         if crawlUrlRes is None:
-            mycursor.execute("INSERT INTO `CrawlUrl` SET `url` = %s", (tagHref, ))
+            mycursor.execute("INSERT INTO `CrawlUrl` SET `url` = %s, `tagId` = %s", (tagHref, tagId,))
             mydb.commit()
-            print(mycursor.lastrowid, "新增 url " + tagHref + "成功")
+            print(mycursor.lastrowid, "發現新文章 - 新增 url " + tagHref + " 成功")
         else:
-            print("url " + tagHref + "已存在")
+            print("文章 - url " + tagHref + "已存在")
 
 # 準備爬取文章
 mycursor.execute("SELECT * FROM `CrawlUrl` WHERE crawled = 'no'")
@@ -67,5 +61,21 @@ for crawlUrl in crawlUrls:
     r = requests.get(domain + crawlUrl['url'], cookies={'over18':'1'})
     soup = BeautifulSoup(r.text, 'html.parser')
     images = soup.find_all(string=re.compile(r'/([\w_-]+[.](jpg|gif|png))$'))
-    print(images)
-    break;
+    tagId = crawlUrl['tagId']
+    articleTitle = re.sub(r'\[.{1,}\]', '', soup.find(property='og:title').get('content')).strip()
+
+    # 新增文章
+    mycursor.execute("INSERT INTO `Article` SET `crawlUrlId` = %s, `title` = %s", (crawlUrl['id'], articleTitle,))
+    articleId = mycursor.lastrowid
+
+    # 新增文章標籤
+    mycursor.execute("INSERT INTO `ArticleTag` SET `articleId` = %s, `tagId` = %s", (articleId, tagId,))
+
+    for img in images:
+        img = str(img)
+        # 新增文章圖片
+        mycursor.execute("INSERT INTO `ArticleImage` SET `articleId` = %s, `img` = %s", (articleId, img,))
+
+    # 更改為已爬取
+    mycursor.execute("UPDATE `CrawlUrl` SET `crawled`='yes' WHERE `id` = %s", (crawlUrl['id'],))
+    mydb.commit()
